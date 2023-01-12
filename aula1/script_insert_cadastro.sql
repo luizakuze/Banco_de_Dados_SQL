f03a2ebd2968036c51527dfa58d62fa9da8aac0b
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-01-11 23:36:15 -0300

    Create script_insert_cadastro.sql

---
+++ aula1/script_insert_cadastro.sql
@@ -0,0 +1,10 @@
+insert into cadastro (Nome,Sobrenome,CPF) Values('Joao','Silva',11111111111);
+insert into cadastro (Nome,Sobrenome,CPF) Values('Juca','Bala',22222222222);
+insert into cadastro (Nome,Sobrenome,CPF) Values('Jose','Cunha',33333333333);
+insert into cadastro (Nome,Sobrenome,CPF) Values('Renato','Souza',4444444444);
+insert into cadastro (Nome,Sobrenome,CPF) Values('Roberto','Ferreira',55555555555);
+insert into cadastro (Nome,Sobrenome,CPF) Values('Valéria','Cunha',66666666666);
+insert into cadastro (Nome,Sobrenome,CPF) Values('Maria','Silva',77777777777);
+insert into cadastro (Nome,Sobrenome,CPF) Values('Jucelia','Pereira',8888888888);
+insert into cadastro (Nome,Sobrenome,CPF) Values('Marcelo','Souza',99999999999);
+insert into cadastro (Nome,Sobrenome,CPF) Values('Fabiana','Ferraz',10101010101);
