3d5d37c257c579a7eaef416e8e707086ab8abafc
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-01-20 14:06:42 -0300

    Create script_insert_produtos.sql

---
+++ aula2/script_insert_produtos.sql
@@ -0,0 +1,10 @@
+insert into produtos (Nome,Valor,Quantidade,id_cad) Values('Laranja',3.50,15,1);
+insert into produtos (Nome,Valor,Quantidade,id_cad) Values('Maça',4.50,4,2);
+insert into produtos (Nome,Valor,Quantidade,id_cad) Values('Abacaxi',7.80,10,2);
+insert into produtos (Nome,Valor,Quantidade,id_cad) Values('Mamão',8.00,20,4);
+insert into produtos (Nome,Valor,Quantidade,id_cad) Values('Banana',6.50,34,3);
+insert into produtos (Nome,Valor,Quantidade,id_cad) Values('Limão',10.00,40,5);
+insert into produtos (Nome,Valor,Quantidade,id_cad) Values('Melancia',7.50,30,6);
+insert into produtos (Nome,Valor,Quantidade,id_cad) Values('Uva',15.00,20,7);
+insert into produtos (Nome,Valor,Quantidade,id_cad) Values('Pera',20.50,5,10);
+insert into produtos (Nome,Valor,Quantidade,id_cad) Values('Tangerina',3.50,21,2);
